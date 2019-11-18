8de3ae6456631f439aff05ad6dd5e3c3f68f269c
hmoon630 <s19017@gsm.hs.kr>
2019-11-18 20:58:39 +0900

    [CREATE] Create Credit Charge API

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 
 import auth from './auth';
+import credit from './credit';
 
 const api = new Router;
 
 api.use('/auth', auth.routes());
+api.use('/credit', credit.routes());
 
 export default api;
