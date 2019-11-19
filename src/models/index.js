336d7b37ebfdc8ea777c4ded674023a9e7869991
hmoon630 <s19017@gsm.hs.kr>
2019-11-19 23:10:31 +0900

    [CREATE] Create Sale Model

--- src/models/index.js
+++ src/models/index.js
@@ -8,6 +8,7 @@ import { Post } from './Post';
 import { Question } from './Question';
 import { Reply } from './Reply';
 import { User_trade } from './User_trade';
+import { Sale } from './Sale';
 
 const config = require(path.join(__dirname, '..', 'config', 'dbconfig.json'))['charger'];
 
@@ -25,5 +26,6 @@ const post = Post(sequelize, Sequelize);
 const question = Question(sequelize, Sequelize);
 const reply = Reply(sequelize, Sequelize);
 const user_trade = User_trade(sequelize, Sequelize);
+const sale = Sale(sequelize, Sequelize);
 
-export { sequelize, Sequelize, account, admin, charge_list, post, question, reply, user_trade};
+export { sequelize, Sequelize, account, admin, charge_list, post, question, reply, user_trade, sale };
