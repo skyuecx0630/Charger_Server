e8c5f7bde209e76eb10bfd83119e4c9214e76de6
hmoon630 <s19017@gsm.hs.kr>
2019-11-19 11:21:20 +0900

    [UPDATE] Update Modify & Delete Question

--- src/api/question/question.ctrl.js
+++ src/api/question/question.ctrl.js
@@ -66,7 +66,7 @@ export const ModifyQuestion = async (ctx) => {
         return;
     }
 
-    //수정하는 사람과 게시글 작성자가 같은가?
+    //존재하는 게시글인가?
     const founded = await question.findOne({
         where : {
             question_code : ctx.request.body.question_code
@@ -74,10 +74,32 @@ export const ModifyQuestion = async (ctx) => {
     })
 
     if (founded == null){
-        console.log(`ModifyQuestion - 수정하는 사람과 게시글 작성자가 다릅니다.`);
+        console.log(`ModifyQuestion - 게시글을 찾을 수 없습니다.`);
         ctx.status = 400;
         ctx.body = {
             "error": "010"
+        }
+        return;
+    }
+
+    //로그인 한 유저는 누구인가?
+    const user = await decodeToken(ctx.header.token);
+
+    if (user == null) {
+        console.log(`ModifyQuestion - 올바르지 않은 토큰입니다.`);
+        ctx.status = 400;
+        ctx.body = {
+            "error": "009"
+        }
+        return;
+    }
+
+    //게시글 작성자와 수정하는 사람이 같은가?
+    if (user.user_code != founded.author){
+        console.log(`ModifyQuestion - 게시글 작성자와 수정하는 사람이 다릅니다.`);
+        ctx.status = 400;
+        ctx.body = {
+            "error": "011"
         }
         return;
     }
@@ -90,6 +112,43 @@ export const ModifyQuestion = async (ctx) => {
     })
 
     console.log(`ModifyQuestion - 게시글이 성공적으로 수정되었습니다.`)
+
+    ctx.status = 200;
+}
+
+export const DeleteQuestion = async (ctx) => {
+    //로그인 한 유저는 누구인가?
+    const user = await decodeToken(ctx.header.token);
+
+    if (user == null) {
+        console.log(`DeleteQuestion - 올바르지 않은 토큰입니다.`);
+        ctx.status = 400;
+        ctx.body = {
+            "error": "009"
+        }
+        return;
+    }
+
+    const founded = await question.findOne({
+        where : {
+            question_code : ctx.request.query.q_code
+        }
+    })
+
+    //게시글 작성자와 삭제 요청자가 일치하는가?
+    if (user.user_code != founded.author) {
+        console.log(`DeleteQuestion - 게시글 작성자와 삭제 요청자가 다릅니다.`);
+
+        ctx.status = 400;
+        ctx.body = {
+            error: "011"
+        };
+        return;
+    }
+
+    await founded.destroy();
+    
+    console.log(`DeleteQuestion - 게시글이 삭제되었습니다.`)
 
     ctx.status = 200;
 }
