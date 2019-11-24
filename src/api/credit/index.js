ec38dcef85ff4eddfa6f908ac517b50973c5db1b
hmoon630 <s19017@gsm.hs.kr>
2019-11-24 13:52:42 +0900

    [UPDATE] Update Show Credit

--- src/api/credit/index.js
+++ src/api/credit/index.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router';
-import { AddCredit, ChargeList } from './credit.ctrl';
+import { AddCredit, ChargeList, ShowCredit } from './credit.ctrl';
 
 const credit = new Router();
 
 credit.get('/list', ChargeList);
+credit.get('/', ShowCredit);
 
 credit.post('/add', AddCredit);
 
